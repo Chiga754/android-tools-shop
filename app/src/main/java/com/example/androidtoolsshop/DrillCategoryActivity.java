f9eacbf194a743d30b2ed8cc3635d9e6892b670a
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-08-28 17:06:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/androidtoolsshop/DrillCategoryActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidtoolsshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DrillCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill_category);
+
+    }
+}
